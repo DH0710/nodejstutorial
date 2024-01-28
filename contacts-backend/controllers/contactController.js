1a6dfcb6e32978d77b46ba058b281605eaaa46c2
DH0710 <DamienHebert710@gmail.com>
2024-01-27 22:37:09 -0600

    added error to createContact function if one of the name phone or email fields are not filled

--- contacts-backend/controllers/contactController.js
+++ contacts-backend/controllers/contactController.js
@@ -25,7 +25,10 @@ const getSingleContact = (req, res) => {
 const createContact = (req, res) => {
     console.log("The request body is :", req.body);
     const {name, email, phone} = req.body;
-    if {}
+    if (!name || !email || !phone ) {
+        res.status(400);
+        throw new Error("All fields are mandatory!")
+    }
     res.status(201).json ({message: "Create new contact"});
 };
 
